7c40a41de23736eba1865425cc3d989c12346467
Wilson Xicará <wilsonx192@gmail.com>
2021-11-29 14:05:00 -0600

    ADD: Implementación inicial de app 'container'.
    - Sección 05, video 42: Assembling the Container
    - Mucha de la configuración inicial es la misma que 'marketing', por lo que se copian los archivos requeridos
    - Se configura la app 'container' para ejecutarse en http://localhost:8080
    - Se define el script "npm run start:dev" para iniciar la app con las configuraciones en DEV.

---
+++ container/src/index.js
@@ -0,0 +1,3 @@
+// Esto solo le da a Webpack (cuando se está ejecutando dentro del navegador) un poco de tiempo para asegurarnos
+// de que podamos cargar todas las dependencias apropiadas para ejecutar realmente nuestro archivo bootstrap.js
+import('./bootstrap');
