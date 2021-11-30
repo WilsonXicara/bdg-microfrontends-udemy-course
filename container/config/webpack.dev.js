997fbd7a29ab60a58302be9b8c1b9955e8e47ab1
Wilson Xicará <wilsonx192@gmail.com>
2021-11-29 18:20:11 -0600

    ADD: ModuleFederationPlugin para conectar 'container' y 'marketing'.
    - Sección 05, video 43: Integration of the Container and Marketing
    - Configuraciones en 'marketing':
      - ModuleFederationPlugin para exponer el archivo 'src/bootstrap' a través del alias 'MarketingApp'.
    - Configuraciones en 'container':
      - ModuleFederationPlugin para intentar recuperar la función mount() de 'marketing' a través del alias 'marketing/MarketingApp'
      - De momento sólo se hace un console.log de la función mount

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
@@ -11,6 +12,14 @@ const devConfig = {
     },
   },
   plugins: [
+    // Definir qué archivos se quieren obtener desde los remotes
+    new ModuleFederationPlugin({
+      name: 'container',  // Aplica solo para 'remotes'. Se agrega por convención
+      remotes: {
+        // ALIAS: 'Marketing_ModuleFederationPlugin.name@HOST:PORT/Marketing_ModuleFederationPlugin.exposes[KEY_OF_FILE]'
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { mount as mountMarketingApp } from 'marketing/MarketingApp';
+
+console.log('>>>> container: mountMarketingApp=', mountMarketingApp);
+
 export default () => {
   return (
     <h1>Hi there!</h1>
