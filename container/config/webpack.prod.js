9acab6d3b91029da949c1173c3b17b661e06348b
Wilson Xicará <wilsonx192@gmail.com>
2021-11-30 17:21:37 -0600

    ADD: Configuración para generar compilado para production para 'container'.
    - Sección 06, video 51: Production Webpack Config for Container
    - Para la compilación se define el siguiente script:
        $ npm run build
    - Configuración de Webpack:
      - Tanto en development como en production se requiere de la plantilla HTML donde se renderizará la app. Motivo por el cual HtmlWebpackPlugin se mueve a la configuración common.
      - Para production se espera la variable de entorno PRODUCTION_DOMAIN para especificar dónde se recuperarán los remotes ('marketing' y demás).
    - Los archivos compilados se crean en el directorio dist/ De momento no se agrega la carpeta a .gitignore porque aún no se define la estrategia para hacer el deploy (que sí o sí requerirá de esta carpeta).

---
+++ container/config/webpack.prod.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',  // Temple to how to name the building files
+  },
+  plugins: [
+    // Definir qué archivos se quieren obtener desde los remotes
+    new ModuleFederationPlugin({
+      name: 'container',  // Aplica solo para 'remotes'. Se agrega por convención
+      remotes: {
+        // ALIAS: 'Marketing_ModuleFederationPlugin.name@HOST:PORT/Marketing_ModuleFederationPlugin.exposes[KEY_OF_FILE]'
+        // Asumimos que los remoteEntry de las distintas app estarán en el mismo dominio pero en subcarpetas
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
