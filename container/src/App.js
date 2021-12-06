f6fb1a0e2b1862cf97a67cd7af2d5dfc72e80b33
Wilson Xicará <wilsonx192@gmail.com>
2021-12-06 17:47:36 -0600

    ADD: Solventando problema de colisión de nombres de clases CSS y probando solución.
    - Sección 09, video 78-79: Fixing Class Name Collisions-Verifying the Fix
    - Se aplica un prefix a los nombres de clases CSS (minificados y casi aleatorios) al compilar para PROD.
    - Otras librerías CSS-in-JS pueden tener una mejor generación de nombres de clases.
    - Otras librerías CSS-in-JS tendrán una forma diferente de personalizar el nombre de clase (consultar la documentación).
    - Validación:
      - Al inspeccionar (Herramientas de desarrollo > Elements) se puede validar que las clases ya no se generan como 'jss1', 'jss2', etc. sino como 'mkt-1', 'mkt-2', etc.
    - Se requiere deploy de las apps (commit a master y cambios en los directorios correspondientes para que se ejecute GitHub Actions).

--- container/src/App.js
+++ container/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import { MarketingApp } from './components/remote-apps';
 import { Header } from './components';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'cnt-',  // Prefix for 'container'
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
