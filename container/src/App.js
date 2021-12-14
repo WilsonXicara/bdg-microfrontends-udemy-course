6aa81c3e4db733e61a8ea5f19e433512944e2cb0
Wilson Xicará <wilsonx192@gmail.com>
2021-12-13 18:40:56 -0600

    ADD: Carga perezosa (Lazy loading) para las remote apps.
    - Sección 11, video 99: Lazily Loading SubApps
    - Cuando el usuario accede a http:localhost:8080/ se obtienen los scripts de todas las remote apps
      - Herramientas de desarrollo (del navegador), tab 'Network', filtrar solo archivos 'JS'
      - Se puede visualizar que se obtiene el bootstrap.js de 'marketing' y 'auth'
    - Esta es la funcionalidad normal pero considerar que cuando el usuario accede al path root en realidad no necesita saber nada aún de 'auth' (sino que hasta que de clic en 'LOGIN').
    - Para optimizar, cuando el usuario accede a http:localhost:8080/ solo debería cargarse el código JS de 'container' y 'marketing', y el de 'auth' se cargaría hasta que de clic en 'LOGIN'
    - Uso de lazy (función) y Suspense (componente) de React para permitir cargar perezosamente diferentes componentes dentro de la app.
     - De esta forma, se recupera el bootstrap.js de 'auth' solo hasta que la app se requiera.

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-import { MarketingApp, AuthApp } from './components/remote-apps';
 import { Header } from './components';
+
+const MarketingAppLazy = lazy(() => import('./components/remote-apps/MarketingApp'));
+const AuthAppLazy = lazy(() => import('./components/remote-apps/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'cnt-',  // Prefix for 'container'
@@ -14,10 +16,12 @@ export default () => {
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <Header />
-        <Switch>
-          <Route path="/auth" component={AuthApp} />
-          <Route path="/" component={MarketingApp} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/auth" component={AuthAppLazy} />
+            <Route path="/" component={MarketingAppLazy} />
+          </Switch>
+        </Suspense>
       </StylesProvider>
     </BrowserRouter>
   );
