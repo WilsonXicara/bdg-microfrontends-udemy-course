cc44bb52c05113e01fba232a4464aaf0e0667c57
Wilson Xicará <wilsonx192@gmail.com>
2021-12-14 16:18:37 -0600

    ADD: Configuración de webpack.common.js para 'dashboard'.
    - Sección 13, video 108.2: Initial Dashboard Setup
    - Definiendo configuración para webpack.common.js en 'dashboard'.
      - Las configuraciones son propias para el proyecto Vue.
    - IMPORTANTE: Hasta este punto el proyecto no es funcional porque no tiene definido los scripts, faltan archivos Page y generaría conflictos con 'auth'.

--- dashboard/config/webpack.common.js
+++ dashboard/config/webpack.common.js
@@ -1,6 +1,41 @@
+const { VueLoaderPlugin } = require('vue-loader') 
+
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  resolve: {
+    // Para indicarle a Webpack que se cargarán archivos .vue
+    extensions: [
+      '.js',
+      '.vue',
+    ],
+  },
   module: {
     rules: [
+      {
+        test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
+        use: [
+          {
+            // Para dar a entender cada vez que intentamos importar una fuente, imagen, etc.
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.scss|.css$/,
+        use: [
+          'vue-style-loader',
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
       {
         // Babel se encarga de procesar el código en cualquier versión y convertirlo en código ECMA5 normal que puede ejecutar fácilmente cualquier navegador
         test: /\.m?js$/,  // Aplica para archivos con extensión 'js'
@@ -10,7 +45,6 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              '@babel/preset-react',
               '@babel/preset-env',
             ],
             plugins: [
@@ -21,4 +55,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
 };
