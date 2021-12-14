03a511065baa364070451310f04d6ea368262212
Wilson Xicará <wilsonx192@gmail.com>
2021-12-14 16:06:20 -0600

    ADD: Duplicando 'auth' en 'dashboard'.
    - Sección 13, video 108.1: Initial Dashboard Setup
    - Copiando la estructura actual del proyecto 'auth' en 'dashboard' ya que la configuración es similar.
    - IMPORTANTE: Hasta este punto el proyecto no es funcional porque no tiene definido los scripts, faltan archivos Page y generaría conflictos con 'auth'.

---
+++ dashboard/src/bootstrap.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';  // react-router-dom internally makes use of this library
+
+import App from './App';
+
+// Mount function to start up the app
+const mount = (element, { onNavigate, onSignIn, defaultHistory, initialPath }) => {
+  const history = defaultHistory || createMemoryHistory({
+    initialEntries: [initialPath],
+  });
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(
+    <App history={history} onSignIn={onSignIn} />,
+    element
+  );
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname: currentPathname } = history.location;
+      if (currentPathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// 1. If we are in development and in isolation, call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  // Assuming our container doesn't have an element with id '_auth-dev-root'
+  const devRoot = document.querySelector('#_auth-dev-root');
+  if (devRoot) {
+    mount(devRoot, {
+      defaultHistory: createBrowserHistory(),
+    });
+  }
+}
+
+// 2. We are running through container and we should export the mount function
+export {
+  mount,
+};
